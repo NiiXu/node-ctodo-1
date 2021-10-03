1b12384f39c2a235ea3c2b6c7e60cc0bfe5e6d23
xu <chenhengxv@outlook.com>
2021-10-03 16:43:22 +0800

    commit

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const db = require('./db.js')
 const inquirer = require('inquirer')
 
 module.exports.add = async (title) => {
-    // 读取之前的任务
+
     const list = await db.read()
-    // 往里面添加一个 title 任务
+
     list.push({title, done: false})
-    // 存储任务到文件
+
     await db.write(list)
 }
 
@@ -94,9 +94,8 @@ function printTasks(list) {
 }
 
 module.exports.showAll = async () => {
-    // 读取之前的任务
+ 
     const list = await db.read()
-    // 打印之前的任务
-    // printTasks
+
     printTasks(list)
 }
